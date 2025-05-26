b4e3e96ef7f58cb307e9ffefcda79e84c5ec1e66
Mustafa Taner Turan <turanmustafa2004@gmail.com>
2025-05-26 16:27:27 +0200

    implemented timer setting display functionality

--- main.ino
+++ main.ino
@@ -1,13 +1,26 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include "AiEsp32RotaryEncoder.h"
 #include "Color.h"
 #include "Led.h"
 #include "ColorButton.h"
 
-void IRAM_ATTR readChoiceEncoderISR();
-void IRAM_ATTR readIncrementEncoderISR();
+void display_timer(const String &buf, int x, int y);
+void display_selector();
+void IRAM_ATTR read_choice_encoder_ISR();
+void IRAM_ATTR read_increment_encoder_ISR();
 void choice_encoder_loop();
 void increment_encoder_loop();
 void color_selection_service();
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C
+#define TIMER_CURSOR_X 64
+#define TIMER_CURSOR_Y 32
 
 #define CHOICE_ENCODER_A_PIN 5
 #define CHOICE_ENCODER_B_PIN 17
@@ -20,6 +33,7 @@ void color_selection_service();
 long hours = 0;
 long minutes = 0;
 long incrementer_selection = 1;
+String timer_str;
 
 const Color RED = {255, 0, 0};
 const Color ORANGE = {230, 200, 0};
@@ -38,6 +52,7 @@ ColorButton yellow_bt(33, YELLOW);
 ColorButton green_bt(25, GREEN);
 AiEsp32RotaryEncoder choice_encoder = AiEsp32RotaryEncoder(CHOICE_ENCODER_A_PIN, CHOICE_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, ROTARY_ENCODER_VCC_PIN, ROTARY_ENCODER_STEPS);
 AiEsp32RotaryEncoder increment_encoder = AiEsp32RotaryEncoder(INCREMENT_ENCODER_A_PIN, INCREMENT_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, ROTARY_ENCODER_VCC_PIN, ROTARY_ENCODER_STEPS);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 //************************************** (main) setup - loop **************************************
 void setup() {
@@ -50,18 +65,34 @@ void setup() {
   green_bt.init_pin();
 
   choice_encoder.begin();
-	choice_encoder.setup(readChoiceEncoderISR);
+	choice_encoder.setup(read_choice_encoder_ISR);
 	choice_encoder.setBoundaries(0, 1, false); // true means values form a circle
   choice_encoder.disableAcceleration();
   choice_encoder.setEncoderValue(incrementer_selection);
 
   increment_encoder.begin();
-	increment_encoder.setup(readIncrementEncoderISR);
+	increment_encoder.setup(read_increment_encoder_ISR);
 	increment_encoder.setBoundaries(0, 59, true);
   increment_encoder.disableAcceleration();
   increment_encoder.setEncoderValue(minutes);
 
   Serial.begin(115200);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  else {
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    timer_str = (hours < 10 ? "0" : "") + String(hours) + ":" + (minutes < 10 ? "0" : "") + String(minutes);
+    display.clearDisplay();
+    display_timer(timer_str);
+    display_selector();
+    display.display();
+  }
 }
 
 void loop() {  
@@ -72,27 +103,54 @@ void loop() {
 }
 
 //************************************** function definitions **************************************
-void IRAM_ATTR readChoiceEncoderISR() {
+void display_timer(const String &buf) {
+    int16_t x1, y1;
+    uint16_t w, h;
+    display.getTextBounds(buf, TIMER_CURSOR_X, TIMER_CURSOR_Y, &x1, &y1, &w, &h); //calc width and height of new string
+    display.setCursor(TIMER_CURSOR_X - (w / 2), TIMER_CURSOR_Y - (h / 2));
+    display.print(buf);
+}
+
+void display_selector() {
+  int16_t x0, y0, x1, y1;
+  uint16_t w, h;
+  display.getTextBounds("00:00", TIMER_CURSOR_X, TIMER_CURSOR_Y, &x1, &y1, &w, &h);
+  if (incrementer_selection == 0) {
+  x0 = TIMER_CURSOR_X - w / 2 - 5;
+  x1 = TIMER_CURSOR_X - 5;
+  } 
+  else {
+  x0 = TIMER_CURSOR_X + w / 2 + 2;
+  x1 = TIMER_CURSOR_X + 2;
+  }
+  y0 = TIMER_CURSOR_Y + (h / 2);
+  y1 = TIMER_CURSOR_Y + (h / 2);
+  display.drawLine(x0, y0, x1, y1, WHITE);
+}
+
+void IRAM_ATTR read_choice_encoder_ISR() {
 	choice_encoder.readEncoder_ISR();
 }
 
-void IRAM_ATTR readIncrementEncoderISR() {
+void IRAM_ATTR read_increment_encoder_ISR() {
 	increment_encoder.readEncoder_ISR();
 }
 
 void choice_encoder_loop() {
 	if (choice_encoder.encoderChanged()) {
     incrementer_selection = choice_encoder.readEncoder();
-		Serial.print("Choice Value: ");
-		Serial.println(incrementer_selection);
     if (incrementer_selection == 1) {
       increment_encoder.setBoundaries(0, 59, true);
       increment_encoder.setEncoderValue(minutes);
     }
     else {
-      increment_encoder.setBoundaries(0, 120, true);
+      increment_encoder.setBoundaries(0, 72, true);
       increment_encoder.setEncoderValue(hours);
     }
+    display.clearDisplay();
+    display_timer(timer_str);
+    display_selector();
+    display.display();
 	}
 }
 
@@ -100,10 +158,11 @@ void increment_encoder_loop() {
   if (increment_encoder.encoderChanged()) {
     if (incrementer_selection == 1) minutes = increment_encoder.readEncoder();
     else hours = increment_encoder.readEncoder();
-    Serial.print("Hour - Minutes: ");
-    Serial.print(hours);
-    Serial.print(" - ");
-    Serial.println(minutes);
+    timer_str = (hours < 10 ? "0" : "") + String(hours) + ":" + (minutes < 10 ? "0" : "") + String(minutes);
+    display.clearDisplay();
+    display_timer(timer_str);
+    display_selector();
+    display.display();
   }
 }
 
